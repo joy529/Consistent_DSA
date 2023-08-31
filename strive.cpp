1b36d30b3e10202ea14b388c529a637913c5689c
joy jain <joyshah204@gmial.com>
2023-08-31 22:58:48 +0530

    till STL - vector iterator

--- strive.cpp
+++ strive.cpp
@@ -539,6 +539,7 @@ void print2(int n) {
 
 
 
+/*
 
 int main() {
     int t;      //how the backend looks, no. of test cases - t
@@ -551,6 +552,9 @@ int main() {
 
     return 0;
 }
+
+*/
+
 
 
 
@@ -575,4 +579,110 @@ int main() {
 
 
 
+
+
+/*
+
+#include<bits/stdc++.h> - library
+
+std::cin >> a;
+std::cout << a;
+
+C++ STL is divided into 4 parts-
+1) Container: vector, queue, list etc., which are used to store data in dynamic memory.    
+2) Algorithm:
+3) Function:
+4) Iterator:
+
+
+*/
+
+
+
+
+
+//--------------Pairs - utility library
+
+
+void explainPair() {
+
+    pair<int, int> p = {1, 3};
+
+    cout << p.first << " " << p.second;
+
+    pair<int, pari<int, int>> p = {1, {3, 4}};
+
+    cout << p.first << " " << p.second.second << " " << p.second.first;
+
+    pair<int, int> arr[] = { {1, 2}, {2, 5}, {5, 1}};
+
+    cout << arr[1].second;
+
+}
+
+
+
+
+
+//--------------Vector
+
+
+void explainVector() {
+
+        //declaration of vector.
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<pair<int, int>> vec;
+
+    vec.push_back({1, 2});
+    vec.emplace_back(1,2);
+
+    vector<int> v(5, 100);      //[100, 100, 100, 100, 100]
+
+    vector<int> v(5);
+
+    vector<int> v1(5, 20);
+    vector<int> v2(v1);
+
+        //access element in the vector.
+
+    vector<int>::iterator it = v.begin();
+
+    it++;
+    cout<< *(it) << " ";
+
+    it = it +  2;
+    cout<< *(it) << " ";
+
+        //use iterator in vector
+
+    vector<int>::iterator it = v.end();
+    vector<int>::iterator it = v.rend();
+    vector<int>::iterator it = v.rbegin();
+
+    cout << v[0] << " " << v.at(0);
+
+    cout << v.back() << " ";
+
+        //printing the vector.
+
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << *(it) << " ";
+    }
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+        cout << *(it) << " ";
+    }
+
+    for (auto it : v) {
+        cout << it << " ";
+    }
+    
+    
+
+}
 
