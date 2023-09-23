45978503921ae8f5d39c1e43958fdbd66c235158
joy jain <joyshah204@gmial.com>
2023-09-23 21:40:14 +0530

    recursion complete

--- strive.cpp
+++ strive.cpp
@@ -1357,6 +1357,117 @@
 
 
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+
+// // Reverse of array using Recursion using one pointer
+
+// void f(int i, int arr[], int n) {
+
+//     if(i >= n/2) return;
+
+//     swap(arr[i], arr[n-i-1]);
+
+//     f(i+1, arr, n);
+// }
+
+
+
+// int main() {
+
+
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for(int i = 0; i < n; i++) cin >> arr[i];
+
+//     f(0, arr, n);
+
+//     for(int i = 0; i < n; i++) cout << arr[i] << " ";
+
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// // recursion , check if a string is palindrom
+
+// bool f(int i, string &s) {
+
+//     if(i >= s.size() / 2) return true;
+
+//     if(s[i] != s[s.size() - i - 1]) return false;
+
+//     return f(i + 1, s);
+// }
+
+// int main() {
+
+//     string s = "madasm";
+
+//     cout << f(0, s);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// // Fibonacci no. - Recursion
+
+// int f(int n) {
+//     if(n <=1) return n;
+//     int last = f(n-1);
+//     int slast = f(n-2);
+
+//     return last + slast;
+// }
+
+// int main() {
+
+//     cout << f(4) << endl;
+
+//     cout << f(4) << "\n";
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// //-----------------------------Hashing---------------------------------------
+
 
 
 
