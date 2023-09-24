1833f0f3596fb94f1f0d642b49f0319ea0e51b3e
joy jain <joyshah204@gmial.com>
2023-09-24 18:26:44 +0530

    Hashing complete

--- strive.cpp
+++ strive.cpp
@@ -1358,6 +1358,7 @@
 
 
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 
@@ -1467,6 +1468,242 @@ using namespace std;
 
 
 // //-----------------------------Hashing---------------------------------------
+
+
+
+
+
+
+
+
+
+
+// // Number Hashing
+
+// int main() {
+
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for(int i = 0; i<n; i++){
+//         cin >> arr[i];
+//     }
+
+//     // precompute 
+//     int hash[13] = {0};
+//     for(int i = 0; i<n; i++) {
+//         hash[arr[i]] += 1;
+//     }
+
+
+//     int q;
+//     cin >> q;
+//     while (q--) {
+//         int number;
+//         cin>>number;
+//         // fetch
+//         cout << hash[number] << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Character Hashing
+
+// int main() {
+
+//     string s;
+//     cin >> s;
+
+//     // pre compute
+//     int hash[26] = {0};
+//     for(int i = 0; i < s.size(); i++) {
+//         hash[(s[i] - 'a')]++;
+//     }
+
+
+//     int q;
+//     cin >> q;
+//     while(q--) {
+//         char c;
+//         cin >> c;
+//         // fetch     
+//         cout<<hash[(c-'a')];
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Code by MAP
+
+// int main() {
+
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i=0 ; i<n ; ++i) {
+//         cin>>arr[i];
+//     }
+
+//     // pre - compute
+//     map<int, int> mpp;
+//     for(int i = 0; i<n; i++) {
+//         mpp[arr[i]]++;
+//     }
+
+//     // iterate in the MAP
+//     // for(auto it : mpp){
+//     //     cout << "key: " << it.first << " -> value: " << it.second <<endl;
+//     // }
+
+//     int q;
+//     cin >> q;
+//     while(q--) {
+//         int number;
+//         cin >> number;
+//         //fetch
+//         cout << mpp[number] << endl;
+//     }
+
+//     return 0;
+// }
+
+ 
+ 
+
+
+
+
+
+
+
+
+// // UnOrdered map
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------Sorting---------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
