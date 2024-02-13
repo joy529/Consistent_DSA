804af1d751d0062cc691045e6bd56fc8a83aad5f
joy jain <joyshah204@gmial.com>
2024-02-13 12:21:29 +0530

    good going

--- strive.cpp
+++ strive.cpp
@@ -1626,109 +1626,294 @@ using namespace std;
 
 
 
+/*
+
 
 // Selection Sort
 
 
-//select min...
+//select min... -> swap
+
+void selection_sort(int arr[], int n) {
+    for(int i = 0; i<=n-2; i++) {
+        int mini = i;
+        for(int j = i; j<=n-1; j++) {
+            if(arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
 
 
 int main() {
 
-    cout << "hello" << endl;
+    // cout << "hello" << endl;
+
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    selection_sort(arr, n);
+
+    for(int i = 0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
+*/
 
 
 
 
+/*
+// Bubble sort
 
+// push ~> max to the last
 
+void bubble_sort(int arr[], int n) {
+    for(int i = n-1; i >= 0; i--) {
+        // int didSwap = 0;
+        for(int j = 0; j <= n-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                // didSwap = 1;
+            } 
+        }
+        // if(didSwap == 0) {
+        //     break;
+        // }
+        // cout << "Runs";
+    }
+}
 
+int main() {
 
+    int n;
+    cin >> n;
 
+    int arr[n];
 
+    for(int i = 0; i<n; i++) cin >> arr[i];
 
+    bubble_sort(arr,n);
 
+    for(int i = 0; i<n; i++) cout << arr[i] << " ";
 
+    return 0;
+}
+*/
 
 
 
+/*
+// insertion sort
 
+// take an element & ~> place it in its correct order.
 
+void insertion_sort(int arr[], int n) {
+    for(int i = 0; i<=n-1; i++) {
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]) {
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            
+            j--;
+            // cout << "Runs";
+        }
+    }
+}
 
+int main() {
 
+    int n;
+    cin >> n;
+    
+    int arr[n];
 
+    for(int i = 0; i<n; i++) cin >> arr[i];
 
+    insertion_sort(arr, n);
 
+    for(int i = 0; i<n; i++) cout << arr[i] << " ";
 
+    return 0;
+}
+*/
 
 
 
 
+/*
+// Merge Sort
+// divide + Merge    - Recursion
 
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
 
+    //storing elements in the temporary array in a sorted manner//
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+
+int main() {
+
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
+
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
+}
+*/
+
+
+
+
+
+/*
+// Quick Sort
+// divide and conqure...
+
+
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> arr) {
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 6, 2, 5, 7, 9, 1, 3};
+    int n = arr.size();
+    cout << "Before Using quick Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr = quickSort(arr);
+    cout << "After Using quick sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
+*/
+
+
+
+
+
+// Array 
+// Largest element in an array
+
+int findLargestElement(int arr[], int n) {
+ 
+  int max = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (max < arr[i]) {
+        max = arr[i];
+    }
+  }
+  return max;
+}
+int main() {
+  int arr1[] = {2,5,1,3,0};
+  int n = 5;
+  int max = findLargestElement(arr1, n);
+  cout << "The largest element in the array is: " << max << endl;
+ 
+  int arr2[] =  {8,10,5,7,9};
+  n = 5;
+  max = findLargestElement(arr2, n);
+  cout << "The largest element in the array is: " << max<<endl;
+  return 0;
+}
